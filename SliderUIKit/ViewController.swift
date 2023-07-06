228e1f1cd57a8566ae92fcacc175d3abf6fe9031
Carrington <Manyuchi>
2023-07-06 14:43:10 +0200

    slider UIKit

--- SliderUIKit/ViewController.swift
+++ SliderUIKit/ViewController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    private let myNewSLider: UISlider = {
+        
+        let slider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.value = 0.5
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        
+        slider.minimumValueImage = UIImage()
+        slider.maximumValueImage = UIImage(systemName: "paperplane")
+        slider.isContinuous = true
+        
+        
+        slider.minimumTrackTintColor = UIColor.lightGray
+        slider.maximumTrackTintColor =  UIColor.red
+        //slider.thumbTintColor = .white
+        return slider
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureViews()
+        configureConstraints()
+    }
+    
+    private func configureViews() {
+        view.addSubview(myNewSLider)
+    }
+    
+    private func configureConstraints() {
+        let myNewSLiderConstraint = [
+            myNewSLider.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            myNewSLider.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            myNewSLider.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 20),
+            
+        ]
+        
+        
+        NSLayoutConstraint.activate(myNewSLiderConstraint)
     }
 
 
