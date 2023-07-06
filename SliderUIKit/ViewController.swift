b69f20e97ade5fd1a49c4ad6290ba1503f276a7e
Carrington <Manyuchi>
2023-07-06 15:50:17 +0200

    added label constraints

--- SliderUIKit/ViewController.swift
+++ SliderUIKit/ViewController.swift
@@ -14,42 +14,68 @@ class ViewController: UIViewController {
         slider.translatesAutoresizingMaskIntoConstraints = false
         slider.value = 0.5
         slider.minimumValue = 0
-        slider.maximumValue = 1
+        slider.maximumValue = 5
+        //slider.backgroundColor = UIColor.white
         
-        slider.minimumValueImage = UIImage()
-        slider.maximumValueImage = UIImage(systemName: "paperplane")
+      //  slider.minimumValueImage = UIImage()
+       // slider.maximumValueImage = UIImage(systemName: "paperplane")
         slider.isContinuous = true
         
-        slider.minimumTrackTintColor = UIColor.lightGray
+        slider.minimumTrackTintColor = UIColor.green
         slider.maximumTrackTintColor =  UIColor.red
-        //slider.thumbTintColor = .white
+        slider.thumbTintColor = .brown
         return slider
     }()
     
-    private let userProfile: UIImageView = {
-        let imageView = UIImageView()
-        return imageView
+    
+    private let myLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.text = "Show the number"
+        return label
     }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViews()
         configureConstraints()
+        
+    }
+    
+    @objc private func sliderAction() {
+        var alpha = myNewSLider.value
+        print("\(alpha)")
+        myLabel.text = "Show the number: \(alpha)"
+        
+        
     }
     
     private func configureViews() {
+        view.addSubview(myLabel)
         view.addSubview(myNewSLider)
+        myNewSLider.addTarget(self, action: #selector(sliderAction), for: .valueChanged)
+        
     }
     
     private func configureConstraints() {
+        
+        let myLabelConstraints = [
+            myLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            myLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 5),
+            myLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -5),
+        ]
+        
         let myNewSLiderConstraint = [
-            myNewSLider.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            myNewSLider.topAnchor.constraint(equalTo: myLabel.bottomAnchor, constant: 30),
             myNewSLider.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            myNewSLider.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 20),
+            myNewSLider.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             
         ]
         
         NSLayoutConstraint.activate(myNewSLiderConstraint)
+        NSLayoutConstraint.activate(myLabelConstraints)
     }
 
 }
